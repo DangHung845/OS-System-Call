fed79f9de9505f6d3426614ff49723e519fb1d7c
Kurumi <pcbninh23@clc.fitus.edu.vn>
2025-03-29 14:13:44 +0700

    minor chanegs

--- xv6-labs/kernel/syscall.c
+++ xv6-labs/kernel/syscall.c
@@ -112,6 +112,7 @@ extern uint64 sys_trace(void);
 // An array mapping syscall numbers from syscall.h
 // to the function that handles the system call.
 static uint64 (*syscalls[])(void) = {
+[0] 0,
 [SYS_fork]    sys_fork,
 [SYS_exit]    sys_exit,
 [SYS_wait]    sys_wait,
@@ -180,7 +181,7 @@ syscall(void)
     // Use num to lookup the system call function for num, call it,
     // and store its return value in p->trapframe->a0
     if(p->trace_mask & (1 << num)) {
-      printf("%d: syscall %s(", p->pid, syscall_names[num - 1]);
+      printf("%d: syscall %s(", p->pid, syscall_names[num]);
       switch (num) {
         case SYS_exit:
         case SYS_kill:
